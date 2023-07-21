07a1851ac072d39483dd968aa8852f418743978b
ajgoras <igorjonczyk99@gmail.com>
2023-07-21 20:09:54 +0200

    added cors to api

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,8 @@ using vue_cat_browser_api.Models;
 using vue_cat_browser_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 
 // Add services to the container.
 builder.Services.Configure<CatsDatabaseSettings>(builder.Configuration.GetSection("CatsDatabase"));
@@ -11,6 +13,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                      policy.WithOrigins("http://127.0.0.1:5173",
+                      "https://127.0.0.1:5173");
+                    });
+});
 
 var app = builder.Build();
 
@@ -26,5 +38,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();

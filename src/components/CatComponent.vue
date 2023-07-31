6156e11cf44edf70ff45269ab354eab09e62d9d9
ajgoras <igorjonczyk99@gmail.com>
2023-07-31 18:53:15 +0200

    added getting cats from api and CatComponent

---
+++ src/components/CatComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { CatType } from '@/types/CatType'
+
+const props = defineProps<{
+  cat: CatType
+}>()
+
+
+</script>
+
+<template>
+  <div class="cat-element">
+    <h2>CAT</h2>
+    <img :id="cat.id" class="cat-photo" alt="cat-photo" draggable="false" :src="cat.image" />
+  </div>
+</template>
+
+<style scoped>
+.cat-element {
+  height: 90vmin;
+  width: 90vmin;
+}
+.cat-photo {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
+</style>
--- src/views/BrowseCatsView.vue
+++ src/views/BrowseCatsView.vue
@@ -1,9 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CatComponent from '@/components/CatComponent.vue'
+import type { CatType } from '@/types/CatType'
+import axios from 'axios'
+import { onBeforeMount, ref, type Ref } from 'vue'
+
+const cats: Ref<CatType[]> = ref([])
+
+const getCats = async () => {
+  const { data } = await axios.get('http://localhost:5143/api/cats')
+  return data
+}
+
+onBeforeMount(async () => {
+  cats.value = await getCats()
+})
+</script>
 
 <template>
   <div id="main">
     <h1>Cats</h1>
+    <div class="cats-container">
+      <div class="cats-list">
+        <p class="cat-name" v-for="cat of cats">{{ cat.name }}</p>
+      </div>
+      <div>
+        <CatComponent :key="cat.id" v-for="cat of cats" :cat="cat"></CatComponent>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#main {
+  margin-top: 50px;
+  margin-left: 50px;
+}
+
+.cats-container {
+  display: grid;
+  grid-template-columns: repeat(2, 30%);
+}
+
+.cats-list {
+  margin-top: 10vmin;
+}
+
+.cat-name {
+  font-size: 6vmin;
+}
+</style>

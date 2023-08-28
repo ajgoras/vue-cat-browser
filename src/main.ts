ff5381ce116ecbc2180a8a64a55d0a2cec679a5a
ajgoras <igorjonczyk99@gmail.com>
2023-08-28 13:26:39 +0200

    installed Vueper Slides

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-
 import './assets/main.css'
-
+import 'vueperslides/dist/vueperslides.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'

54ed6cbee3a3165d02e2902ee7502e9503191591
ajgoras <igorjonczyk99@gmail.com>
2023-09-02 16:27:21 +0200

    added mobile styles to TutorialComponent

--- src/components/TutorialComponent.vue
+++ src/components/TutorialComponent.vue
@@ -61,13 +61,45 @@ onMounted(() => {
 }
 .cat-image {
   margin-left: 20px;
-  height: 30vmax;
+  height: 35vmax;
   width: auto;
 }
 .pointer-image {
   height: 10vmax;
   position: absolute;
-  top: 17vmax;
-  left: 35vmax;
+  top: 23vmax;
+  left: 41vmax;
+}
+
+@media (max-width: 760px) {
+  #TutorialComponent {
+    grid-template-columns: auto;
+    grid-template-rows: 1fr 3fr;
+    margin-top: 130px;
+    padding: 6vmax 4vmin 12vmax 4vmin;
+  }
+  h1 {
+    font-size: 7vmin;
+  }
+
+  h2 {
+    font-size: 4vmin;
+  }
+
+  .cat-image {
+    margin-left: 0;
+    height: 55vmin;
+  }
+  .image-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+  }
+  .pointer-image {
+    height: 7vmax;
+    position: absolute;
+    top: 37vmin;
+    left: 65vmin;
+  }
 }
 </style>
